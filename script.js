bfca386d1aaf6615d9ea22e4f6dcb161df056d13
Ayush <ayushjha090@gmail.com>
2022-07-12 23:19:30 +0530

    Added basic functionality of calculator using javascript.

--- script.js
+++ script.js
@@ -13,8 +13,55 @@ for(let btn of buttons){
 
 function calculator(value){
     if(value == '+' || value == '-' || value == '*' || value == '/' || value == '=' || value == 'AC' || value == '+/-' || value == '%'){
-        console.log(value);
+        if(value == 'AC'){
+            display.innerText = "";
+        }else if(value == '+/-'){
+            operand1 = parseFloat(display.textContent);
+            operand1 = (-1) * operand1;
+            display.innerText = operand1;
+        }
+        else{
+            switch (value) {
+                case '+':
+                    operand1 = parseFloat(display.textContent);
+                    display.innerText = "";
+                    operator = '+';
+                    break;
+                case '-':
+                    operand1 = parseFloat(display.textContent);
+                    display.innerText = "";
+                    operator = '-';
+                    break;
+                case '*':
+                    operand1 = parseFloat(display.textContent);
+                    display.innerText = "";
+                    operator = '*';
+                    break;
+                case '/':
+                    operand1 = parseFloat(display.textContent);
+                    display.innerText = "";
+                    operator = '/';
+                    break;
+                case '=':
+                    operand2 = parseFloat(display.textContent);
+                    display.innerText = "";
+                    let displayVal = evaluateExpression(operand1, operand2, operator);
+                    display.innerText = displayVal;
+                default:
+                    break;
+            }
+
+        }  
     }else{
         display.innerText += value;
     }
+}
+
+function evaluateExpression(operand1, operand2, operator){
+    let expression = operand1 + " " + operator + " " + operand2;
+    let value = eval(expression);
+    if(value == 'Infinity'){
+        return 'Error';
+    }
+    return value;
 }
