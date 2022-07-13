e2e60d45498ec8b81e112bb8977dce29dae11f98
Ayush <ayushjha090@gmail.com>
2022-07-13 22:51:28 +0530

    Completed Calculator on click functionality using javascript.

--- script.js
+++ script.js
@@ -12,47 +12,36 @@ for(let btn of buttons){
 }
 
 function calculator(value){
-    if(value == '+' || value == '-' || value == '*' || value == '/' || value == '=' || value == 'AC' || value == '+/-' || value == '%'){
-        if(value == 'AC'){
-            display.innerText = "";
-        }else if(value == '+/-'){
-            operand1 = parseFloat(display.textContent);
-            operand1 = (-1) * operand1;
-            display.innerText = operand1;
+    if(value == '+' || value == '-' || value == '*' || value == '/'){
+        // Fetching the operand1 and operator
+        operand1 = parseFloat(display.textContent);
+        operator = value;
+        display.innerText = "";
+    }else if(value == '%'){
+        // Calculate the percentage
+        operand1 = parseFloat(display.textContent);
+        display.innerText = "";
+        operand1 = operand1/100;
+        display.innerText = operand1;
+    }else if(value == '+/-'){
+        // Converts positive number to negative number and vice-versa
+        operand1 = parseFloat(display.textContent);
+        operand1 = (-1)*operand1;
+        display.innerText = operand1;
+    }else if(value == '='){
+        // Calculates the expression value by fetching operand2
+        operand2 = parseFloat(display.textContent);
+        display.innerText = "";
+        operand1 = evaluateExpression(operand1, operand2, operator);
+        if(operand1 != 'Error'){
+            operand1 = Math.round(operand1*100)/100;
         }
-        else{
-            switch (value) {
-                case '+':
-                    operand1 = parseFloat(display.textContent);
-                    display.innerText = "";
-                    operator = '+';
-                    break;
-                case '-':
-                    operand1 = parseFloat(display.textContent);
-                    display.innerText = "";
-                    operator = '-';
-                    break;
-                case '*':
-                    operand1 = parseFloat(display.textContent);
-                    display.innerText = "";
-                    operator = '*';
-                    break;
-                case '/':
-                    operand1 = parseFloat(display.textContent);
-                    display.innerText = "";
-                    operator = '/';
-                    break;
-                case '=':
-                    operand2 = parseFloat(display.textContent);
-                    display.innerText = "";
-                    let displayVal = evaluateExpression(operand1, operand2, operator);
-                    display.innerText = displayVal;
-                default:
-                    break;
-            }
-
-        }  
+        display.innerText = operand1;
+    }else if(value == 'AC'){
+        // Clears the display of the Calculator
+        display.innerText = "";
     }else{
+        // Appends numeric values to the display
         display.innerText += value;
     }
 }
@@ -60,7 +49,8 @@ function calculator(value){
 function evaluateExpression(operand1, operand2, operator){
     let expression = operand1 + " " + operator + " " + operand2;
     let value = eval(expression);
-    if(value == 'Infinity'){
+    // Check condition for Divide by zero error
+    if(value == 'Infinity' || isNaN(value)){
         return 'Error';
     }
     return value;
